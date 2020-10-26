feebf1bdbca0e54300230b8397f97d1d8ffbbdc4
Nisha271 <bukkenisha@gmail.com>
2020-10-26 16:24:56 +0530

    [Archana] Added: UC5 for getting the stake for win or loss per day for month

--- src/com/gamblersimulation/GamblerSimulation.java
+++ src/com/gamblersimulation/GamblerSimulation.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class GamblerSimulation {
 	public static final int  STAKE = 100 ;
 	public static final int  BET = 1 ;
-	public static int cash,lossCondition,winCondition,totalAmount=0;
+	public static int cash,lossCondition,winCondition,totalAmount=0,daysLoss,daysWins;
 	public static void betting(){
 		Random r = new Random();
 		int number = r.nextInt(2);
@@ -12,16 +12,19 @@ public class GamblerSimulation {
 			cash -=1;
 		}
 	}
-	public int stopStake(){
+	public int stopStake(int day){
 		lossCondition = (int) Math.round(STAKE * 0.5);
 		winCondition = (int) Math.round(STAKE + (STAKE * 0.5));
 		boolean stop =true;
+		cash = STAKE;
 		while(stop == true){
 			betting();
 			if(cash == lossCondition){
+				System.out.println( " Loss " +"Day : "+ day +" Stake : " + cash);
 				stop=false;
 			}
 			if(cash == winCondition){
+				 System.out.println( " Win " +"Day : "+ day +" Stake : " + cash);
 				stop=false;
 			}
 		}
@@ -33,18 +36,22 @@ public class GamblerSimulation {
 		int day = 0;
 		while(daysPlaying >0){
 			day++;
-			day_stake=stopStake();
+			day_stake=stopStake(day);
 			daysPlaying=daysPlaying - 1;
 			totalAmount=totalAmount+day_stake;
 		}
 		System.out.println("Total Amount Earned or Loss by Gambler at end : " + totalAmount);
 		return totalAmount;
 	}
+	public int calculateForMonth(){
+		getTotalAmountWinorLoss();
+		return totalAmount;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//System.out.print(stake +" "+bet);
 		GamblerSimulation gs = new GamblerSimulation();
-		System.out.println("Stake : " + gs.getTotalAmountWinorLoss());
+		System.out.println("Stake : " + gs.calculateForMonth());
 	}
 
 }
