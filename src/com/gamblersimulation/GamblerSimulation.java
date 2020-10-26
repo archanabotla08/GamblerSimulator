8c641641eaf67dfa9ea5aadbb7baabdc4b1f6eb2
Nisha271 <bukkenisha@gmail.com>
2020-10-26 09:15:57 +0530

    [Archana] Added: Gambler Simulator file

---
+++ src/com/gamblersimulation/GamblerSimulation.java
@@ -0,0 +1,11 @@
+package com.gamblersimulation;
+
+public class GamblerSimulation {
+	public static int stake =100 ;
+	public static int bet =1 ;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print(stake +" "+bet);
+	} 
+
+}
