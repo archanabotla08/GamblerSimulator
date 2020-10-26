4f1debb8fe6f8ac20acc5dd21347fd68ceec8efa
Nisha271 <bukkenisha@gmail.com>
2020-10-26 09:36:42 +0530

    [Archana] Added: UC2 with win or loss

--- src/com/gamblersimulation/GamblerSimulation.java
+++ src/com/gamblersimulation/GamblerSimulation.java
@@ -1,11 +1,24 @@
-package com.gamblersimulation;
-
+import java.util.*;
 public class GamblerSimulation {
-	public static int stake =100 ;
-	public static int bet =1 ;
+	public static final int  STAKE =100 ;
+	public static final int  BET =1 ;
+	public static int wins = 0;
+	public static int loss = 0;
+	public static void betting(){
+		Random r = new Random();
+		int number = r.nextInt(2);
+		if(number == 1){
+			wins +=1;
+		}else{
+			loss +=1;
+		}
+		System.out.println("Win : " + wins + " Loss :" + loss);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print(stake +" "+bet);
+		//System.out.print(stake +" "+bet);
+		GamblerSimulation gs = new GamblerSimulation();
+		gs.betting();
 	} 
 
 }
